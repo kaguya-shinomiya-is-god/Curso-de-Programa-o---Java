7412e2bca93ff40275a163afee99b830768dd8cf
ensinhu <90516509+kaguya-shinomiya-is-god@users.noreply.github.com>
2022-03-30 13:13:13 -0300

    Update Main.java

--- Atividade1/Main.java
+++ Atividade1/Main.java
@@ -7,6 +7,10 @@ public class Main {
    */
 
    public static void main(String[] args) {
+      
+      //System.out.println("Hello World");
+      
+      //Codigo da situação problema
        
     Scanner input = new Scanner(System.in);
     
