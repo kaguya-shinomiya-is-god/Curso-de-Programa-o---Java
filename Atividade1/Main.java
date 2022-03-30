e1eaf89b6b8a73312c0e7b700c3b5c9b4e827799
ensinhu <90516509+kaguya-shinomiya-is-god@users.noreply.github.com>
2022-03-30 13:09:53 -0300

    Add files via upload

---
+++ Atividade1/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+   /* Sobre mim:
+   Meu nome e Enzo Rangel Dias Lima, tenho 14 e curto muito jogar e tecnologias eletronicas em geral, porem sou mais chegado a
+   softwares e programaçao, e sonho trabalhar na area algum dia.
+   */
+
+   public static void main(String[] args) {
+       
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Bem vindo a definidor de area e perimetro de um retangulo :)");
+    System.out.println("Digite a base de seu retangulo em centimetros:");
+    int base = input.nextInt(); //input do valor da base
+    System.out.println("Digite a altura de seu retangulo em centimetros");
+    int alt = input.nextInt(); //input no valor da altura
+    System.out.println("O perimetro do retangulo e: " + ((base*2) + (alt*2)) + "cm");
+    System.out.println("A area do retangulo e: " + base*alt + "cm²");
+    
+    input.close();
+   }
+}
